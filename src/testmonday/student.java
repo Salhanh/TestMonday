2503eff2c944b6de5b4ee9d1a26d294e23c3b591
hetal <hetal@10.0.0.215>
2024-06-08 03:15:38 -0400

    Test monday version 1

---
+++ src/testmonday/student.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package testmonday;
+
+/**
+ *
+ * @author hetal
+ */
+public class student {
+    private int id;
+    private String name;
+    
+}
